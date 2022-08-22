18a5544fb1352b1f68e0e6524edc34c0fe3244e8
PrattayDhar <prattaygub@gmail.com>
2022-08-22 15:53:28 +0100

    claculation

---
+++ Common.js
@@ -0,0 +1,13 @@
+function topFive(Player, buttonPlayer) {
+    const len = document.getElementsByTagName('li').length;
+    if (len < 5) {
+        const list = document.createElement('li');
+        const name = document.getElementById(Player).innerText;
+        list.innerText = name;
+        const ol = document.getElementById("list");
+        ol.appendChild(list);
+        document.getElementById(buttonPlayer).disabled = true;
+    }
+    else
+        alert('Your Limit Only Top 5')
+}
--- Script.js
+++ Script.js
@@ -1,51 +1,39 @@
 
 document.getElementById("player-button").addEventListener('click', function () {
-  
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name')
-
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
+    topFive('player-name', 'player-button');
 })
-document.getElementById("player-button2").addEventListener('click', function () {
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name2')
 
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
-    document.getElementById("player-button2").disabled = true;
+document.getElementById("player-button2").addEventListener('click', function () {
+    topFive('player-name2', 'player-button2');
 })
 document.getElementById("player-button3").addEventListener('click', function () {
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name3')
-
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
+    topFive('player-name3', 'player-button3');
 })
 document.getElementById("player-button4").addEventListener('click', function () {
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name4')
-
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
+    topFive('player-name4', 'player-button4');
 })
 document.getElementById("player-button5").addEventListener('click', function () {
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name5')
-
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
+    topFive('player-name5', 'player-button5');
 })
 document.getElementById("player-button6").addEventListener('click', function () {
-    const ul = document.getElementById("list");
-    const li = document.createElement("li");
-    const name = document.getElementById('player-name6')
-
-    li.appendChild(document.createTextNode(name.innerText));
-    ul.appendChild(li);
+    topFive('player-name6', 'player-button6');
 })
+
+// BUDGET CALCULATION
+
+document.getElementById('FirstCalculation').addEventListener('click', function(){
+    const oldinput = document.getElementById('playerinput').value;
+    const newinput = parseInt(oldinput);
+    const len = document.getElementsByTagName('li').length;
+    const cost= newinput*len;
+    const textfild= document.getElementById('fild');
+    textfild.innerText=cost;
+    
+})
+
+
+
+
+
+
+
